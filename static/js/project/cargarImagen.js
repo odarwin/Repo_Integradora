11bd47cca306694155f2ae47c20318bfade6fbdf
odarwin <oguaman@espol.edu.ec>
2022-01-27 05:34:35 -0500

    actualizando

--- static/js/project/cargarImagen.js
+++ static/js/project/cargarImagen.js
@@ -9,9 +9,6 @@ $(document).ready(function () {
 		//get the file name
 		var fileName = $(this).val();
 		fileName = fileName.replace("C:\\fakepath\\", "");
-		// var ruta = "C:-Users-Darwin-Documents-PROYECTOS-Integradora-DeteccionPark-Papaya-images-"
-		// fileName = ruta + fileName
-		//replace the "Choose a file" label
 		$(this).next(".custom-file-label").html(fileName);
 	});
 });
